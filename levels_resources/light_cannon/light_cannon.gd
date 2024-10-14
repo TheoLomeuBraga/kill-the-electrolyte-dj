057d85f51c3bc89c827ea2381f651fc2bb19779c
Théo <63592879+TheoLomeuBraga@users.noreply.github.com>
2024-10-14 10:20:57 -0300

    a

--- levels_resources/light_cannon/light_cannon.gd
+++ levels_resources/light_cannon/light_cannon.gd
@@ -1,14 +1,18 @@
 extends Node3D
 
 
+@export var normal_light : bool = false
 var rng : RandomNumberGenerator = RandomNumberGenerator.new()
 func _ready() -> void:
-	$SpotLight3D.light_color.s = 1
-	$SpotLight3D.light_color.v = 1
-	
-	$SpotLight3D.light_color.h = rng.randf_range(0,1)
+	if not normal_light:
+		$SpotLight3D.light_color.s = 1
+		$SpotLight3D.light_color.v = 1
+		$SpotLight3D.light_color.h = rng.randf_range(0,1)
+	else:
+		$SpotLight3D.light_color = Color.WHITE
 
 func _process(delta: float) -> void:
-	$SpotLight3D.light_color.h += delta / 5
-	if $SpotLight3D.light_color.h > 1:
-		$SpotLight3D.light_color.h -= 1
+	if not normal_light:
+		$SpotLight3D.light_color.h += delta / 5
+		if $SpotLight3D.light_color.h > 1:
+			$SpotLight3D.light_color.h -= 1
