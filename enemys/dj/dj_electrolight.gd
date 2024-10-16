b3126ba9d0c0f27d049a39a0a7aaa717a14604fb
Théo <63592879+TheoLomeuBraga@users.noreply.github.com>
2024-10-16 17:36:46 -0300

    a

--- enemys/dj/dj_electrolight.gd
+++ enemys/dj/dj_electrolight.gd
@@ -1,5 +1,13 @@
 extends StaticBody3D
 
-
+var death : bool = false
 func hit(damage :int):
+	if death == false:
+		$js_electrolight.visible = false
+		$GPUParticles3D.emitting = true
+		death = true
+	
+
+func _on_gpu_particles_3d_finished() -> void:
 	Global.contract_complete()
+	queue_free()
